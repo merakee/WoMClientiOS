156f79b812c8d3497558d7cb6ef6d9d4946b0a62
Kevin Moy <kevintmoy@gmail.com>
2014-10-06 14:00:05 -0700

    location feature

--- WordOfMouthEngine/LocationViewController.h
+++ WordOfMouthEngine/LocationViewController.h
@@ -4,10 +4,23 @@
 //
 //  Created by Kevin Moy on 10/3/14.
 //  Copyright (c) 2014 Bijit Halder. All rights reserved.
-//
 
 #import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
 
-@interface LocationView : UIViewController
+//extern const CLLocationAccuracy kCLLocationAccuracyBestForNavigation;
+//extern const CLLocationAccuracy kCLLocationAccuracyBest;
+//extern const CLLocationAccuracy kCLLocationAccuracyNearestTenMeters;
+//extern const CLLocationAccuracy kCLLocationAccuracyHundredMeters;
+//extern const CLLocationAccuracy kCLLocationAccuracyKilometer;
+//extern const CLLocationAccuracy kCLLocationAccuracyThreeKilometers;
+
+//locationAccuracy = kCLLocationAccuracyThreeKilometers;
+static const int minimumDistance = 1000;
+
+@interface LocationViewController : UIViewController
+@property (assign, nonatomic) CLLocationDistance *distanceFilter;
+@property (assign, nonatomic) CLLocationAccuracy *desiredAccuracy;
+@property (nonatomic, strong) CLLocationManager *locationManager;
 
 @end
